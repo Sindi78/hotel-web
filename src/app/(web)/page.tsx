c1b6735b9bd23a0fe47846f076f77d539edc675d
Anggia Putri <2203010031@unper.ac.id>
2025-05-21 02:20:00 +0700

    menampilkan section featured room di halaman landing page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,13 +1,18 @@
 import Gallery from "@/components/Gallery/Gallery";
+import FeaturedRoom from '@/components/FeaturedRoom/FeaturedRoom';
 import HeroSection from "@/components/HeroSection/HeroSection";
 import NewsLetter from "@/components/NewsLetter/NewsLetter";
 import PageSearch from "@/components/PageSearch/PageSearch";
+import { getFeaturedRoom } from '@/libs/apis';
 
-const Home = () => {
+const Home = async () => {
+  const featuredRoom = await getFeaturedRoom();
+
   return (
     <>
       <HeroSection />
       <PageSearch />
+      <FeaturedRoom featuredRoom={featuredRoom} />
       <Gallery />
       <NewsLetter />
     </>
